5d1348249eb9f29c9a3ec76ded4150147aae7688
Aleks Bozhilov <aleks_bozhilov@starsgroup.com>
2021-07-16 00:29:45 +0300

    Refactoring

--- test_img_detection.py
+++ test_img_detection.py
@@ -1,16 +1,18 @@
 import face_detect_alex
 import unittest
+import os
 
 # Testing
-test_img_1 = "/Users/bozhiale/PycharmProjects/pythonProject/18_faces.png"
-test_img_2 = "/Users/bozhiale/PycharmProjects/pythonProject/11_bulgarian_team.png"
-test_img_3 = "/Users/bozhiale/PycharmProjects/pythonProject/4_abba.png"
+image_path = os.getcwd()
+test_img_1 = "/18_faces.png"
+test_img_2 = "/11_bulgarian_team.png"
+test_img_3 = "/4_abba.png"
 eleven = 11
 eighteen = 18
 four = 4
 class ImageFaceRecognitionTests(unittest.TestCase):
 
     def test_01_image_recognition(self):
-        self.assertEqual((eleven, eighteen, four), (face_detect_alex.face_detect(test_img_2, eleven),
-                                        face_detect_alex.face_detect(test_img_1, eighteen),
-                                        face_detect_alex.face_detect(test_img_3, four)))
+        self.assertEqual((eleven, eighteen, four), (face_detect_alex.face_detect(image_path + test_img_2, eleven),
+                                        face_detect_alex.face_detect(image_path + test_img_1, eighteen),
+                                        face_detect_alex.face_detect(image_path + test_img_3, four)))
